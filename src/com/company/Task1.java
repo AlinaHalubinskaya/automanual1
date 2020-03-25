d2d45e00546fca5c6ffddb37d0f28c2400468213
ahalubinskaya <alina.halubinskaya@bglgroup.co.uk>
2020-03-25 17:46:00 +0300

    1 success

---
+++ src/com/company/Task1.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+
+        int f = a * (b + c) + d * e;
+        System.out.println(f);
+
+
+    }
+}
---
+++ src/com/company/Task2.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Task2 {
+
+        public static void main(String[] args) {
+            double p = 3.14;
+            int r = 42;
+
+            double S = p * r * r;
+            System.out.println(S);
+
+
+        }
+    }
+
