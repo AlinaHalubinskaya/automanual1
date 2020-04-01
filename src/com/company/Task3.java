dc9a971808ec8a3d4caf5a24755f8158252480de
ahalubinskaya <alina.halubinskaya@bglgroup.co.uk>
2020-04-02 00:06:28 +0300

    Task #3

--- src/com/company/Task3.java
+++ src/com/company/Task3.java
@@ -3,6 +3,8 @@ package com.company;
 public class Task3 {
     public static void main(String[] args) {
 
+        System.out.print("First variant" + "\n" );
+
        for (int Forward = 1; Forward <= 99; Forward += 2) {
                 System.out.print(" " + Forward);
             }
@@ -11,5 +13,19 @@ public class Task3 {
        for (int Backward = 99; Backward >= 1; Backward -= 2) {
                 System.out.print(" " + Backward);
             }
+        System.out.print(" \n" + "Second variant"+ "\n" );
+
+        for(int i = 1; i <= 99; i++ ) {
+           if(i%2==1) {
+               System.out.print(" " + i);
+           }
+       }
+        System.out.print(" \n");
+
+        for(int i = 99; i >= 1; i-- ) {
+            if(i%2==1) {
+                System.out.print(" " + i);
+            }
+        }
     }
 }
